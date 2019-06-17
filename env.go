0557e507afed53cb3c7026ca5be1c0a7e5d57c7b
jmromanos <jmromanos96@gmail.com>
2019-06-17 21:35:14 +0200

    #31 -Added the option to load env postman file. Fix some linting issues

--- env.go
+++ env.go
@@ -1,5 +1,11 @@
 package tranquility
 
+import (
+	"encoding/json"
+	"io/ioutil"
+	"os"
+)
+
 type environment map[string]string
 
 //Env is the global enviroment to be shared across actions
@@ -17,12 +23,42 @@ func (env environment) Get(key string) string {
 
 // Del deletes the key and the value associated from the map
 func (env environment) Del(key string) {
-	delete(env,key)
+	delete(env, key)
 }
 
 // Init initializes the environment with the given map
 func (env environment) Init(initMap map[string]string) {
-	for k,v := range initMap {
-		env.Set(k,v)
+	for k, v := range initMap {
+		env.Set(k, v)
+	}
+}
+
+// Representation of a Postman Environment json file
+type postmanEnv struct {
+	Values []postmanEnvVar `json:"values"`
+}
+
+// postmanEnvVar is the representation of a Postman environment value from a json
+type postmanEnvVar struct {
+	Key   string `json:"key"`
+	Value string `json:"value"`
+}
+
+// ReadPostmanEnv initializes the environment with the given json format postman environment file
+func (env environment) ReadPostmanEnv(file string) {
+	envFile, err := os.Open(file)
+	if err != nil {
+		panic(err)
+	}
+	defer envFile.Close()
+	jsonBytes, err := ioutil.ReadAll(envFile)
+	if err != nil {
+		panic(err)
+	}
+	var loadedEnv postmanEnv
+	json.Unmarshal(jsonBytes, &loadedEnv)
+
+	for i := range loadedEnv.Values {
+		env.Set(loadedEnv.Values[i].Key, loadedEnv.Values[i].Value)
 	}
 }
