d966031d37983361e3fa59099d0cca254cf2d993
jmromanos <jmromanos96@gmail.com>
2019-07-24 20:39:36 +0200

    48 -Fix panic error in case parameters or headers aren't init before

--- request.go
+++ request.go
@@ -7,8 +7,19 @@ type Request struct {
 	Test      func(res *Response)
 }
 
+//verifyAction initializes parameters and headers if they are null to avoid errors with PreActions
+func verifyAction(req *Request) {
+	if req.Action.Parameters == nil {
+		req.Action.Parameters = map[string]string{}
+	}
+	if req.Action.Headers == nil {
+		req.Action.Headers = map[string]string{}
+	}
+}
+
 //Run perfoms the full request in the following order PreAction, Action and Test
 func (req Request) Run() {
+	verifyAction(&req)
 	if req.PreAction != nil {
 		req.PreAction(req.Action)
 	}
